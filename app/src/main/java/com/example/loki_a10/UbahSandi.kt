e63485ec0249ada270fcfd831752dd2c5c12a69f
2011521013_PutiJugalo <2011521013_puti@student.unand.ac.id>
2022-09-20 16:07:26 +0700

    layout puti

---
+++ app/src/main/java/com/example/loki_a10/UbahSandi.kt
@@ -0,0 +1,11 @@
+package com.example.loki_a10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UbahSandi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ubah_sandi)
+    }
+}
