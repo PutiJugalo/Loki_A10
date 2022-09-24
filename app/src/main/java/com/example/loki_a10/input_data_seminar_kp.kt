869ee398305f3336083a80078627d2c544ae0e6d
ShefillaRaisya <raisyashefilla488@gmail.com>
2022-09-24 23:22:31 +0700

    LALA LAAYOUT

---
+++ app/src/main/java/com/example/loki_a10/input_data_seminar_kp.kt
@@ -0,0 +1,11 @@
+package com.example.loki_a10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class input_data_seminar_kp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_data_seminar_kp)
+    }
+}
