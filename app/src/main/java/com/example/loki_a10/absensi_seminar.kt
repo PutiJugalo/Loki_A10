0e3467ffd5de483d950fc786bfec1771bc8c4931
bobager03 <luthfiahumaira03@gmail.com>
2022-10-13 16:00:12 +0700

    luthfia update

--- app/src/main/java/com/example/loki_a10/absensi_seminar.kt
+++ app/src/main/java/com/example/loki_a10/absensi_seminar.kt
@@ -1,7 +1,9 @@
 package com.example.loki_a10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class absensi_seminar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
