3c5083b0d12b4b907b3402f45af22683ce6a6537
bobager03 <luthfiahumaira03@gmail.com>
2022-10-21 12:26:21 +0700

    bismillah uts

--- app/src/main/java/com/example/loki_a10/InputSeminarKPActivity.kt
+++ app/src/main/java/com/example/loki_a10/InputSeminarKPActivity.kt
@@ -1,26 +1,23 @@
 package com.example.loki_a10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 
-class DataSeminarKpActivity : AppCompatActivity() {
+class InputSeminarKPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_seminar_kp)
+        setContentView(R.layout.activity_input_data_seminar_kp)
 
         supportActionBar?.hide()
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setHomeButtonEnabled(true)
     }
-    override fun onSupportNavigateUp(): Boolean {
-        finish()
-        return true
-    }
 
-    fun dataSeminarClicked(view: View) {
-        Toast.makeText(this, "Data Seminar KP berhasil ditambahkan", Toast.LENGTH_SHORT).show()
+    fun ke_dataPesertaSeminarKP(view: View) {
+        intent = Intent(this, DataSeminarKpActivity::class.java)
+        startActivity(intent)
     }
 }
