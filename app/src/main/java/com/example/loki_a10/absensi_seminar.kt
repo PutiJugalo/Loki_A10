ef706ef600950cbaef82ea47afb19cedebf1e5eb
ShefillaRaisya <raisyashefilla488@gmail.com>
2022-12-19 18:30:17 +0700

    notifikasi absensi

--- app/src/main/java/com/example/loki_a10/absensi_seminar.kt
+++ app/src/main/java/com/example/loki_a10/absensi_seminar.kt
@@ -1,15 +1,22 @@
 package com.example.loki_a10
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.loki_a10.databinding.ActivityAbsensiSeminarBinding
 import com.example.loki_a10.databinding.ActivityDetailKpBinding
 import android.widget.Toast
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.android.synthetic.main.activity_absensi_seminar.*
 
 class absensi_seminar : AppCompatActivity() {
+    private lateinit var notificationManager: NotificationManagerCompat
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityAbsensiSeminarBinding
 
@@ -20,6 +27,21 @@ class absensi_seminar : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        //notifikasi
+        notificationManager = NotificationManagerCompat.from(this)
+
+        FirebaseApp.initializeApp(this)
+        FirebaseMessaging.getInstance().subscribeToTopic("E-KP") //THis for by topic
+            .addOnCompleteListener { task ->
+                var msg = "success"
+                if (!task.isSuccessful) {
+                    msg = "gagal"
+                }
+                Log.d(ContentValues.TAG, msg)
+            }
+        buttonHadir.checkSessionStatus()
+        //------
 
         val bundle : Bundle? = intent.extras
         val namaMhsLain = bundle!!.getString("namaMhsLain")
